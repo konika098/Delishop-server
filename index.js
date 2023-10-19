b304dcf286fc53714bd2ff7c9af755a25132071a
konika098 <konikaakter2000@gmail.com>
2023-10-19 21:57:03 +0600

    fixed my error issues

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const express = require ('express');
-const cors = require ('cors');
-const app =express()
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 5000
 
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.get(express.json());
+app.use(express.json());
+
+// const uri =`mongodb+srv://DeliShop:bPOceElhAFu7rA7V@cluster0.0yl10xg.mongodb.net/?retryWrites=true&w=majority;` 
+var uri = "mongodb://DeliShopSer:cRdcgpesbuMuYOim@ac-eadvqhz-shard-00-00.ycrlcva.mongodb.net:27017,ac-eadvqhz-shard-00-01.ycrlcva.mongodb.net:27017,ac-eadvqhz-shard-00-02.ycrlcva.mongodb.net:27017/?ssl=true&replicaSet=atlas-hbjutf-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 
 
-
-
-
-
-const uri =`mongodb+srv://${process.env.S3_BUCKET}:${process.env.S3_KEY}@cluster0.ycrlcva.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -28,24 +24,20 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+   
   }
 }
+
 run().catch(console.dir);
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
 
-
-
-app.get('/', (req,res)=>{
-    res.send('server is running')
-})
-app.listen(port, ()=>{
-    console.log(`delishop is comming run${port}`)
-})
+app.listen(port, () => {
+  console.log(`delishop is coming, running on port ${port}`);
+});
